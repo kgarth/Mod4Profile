a56b7f23c66da889551f2182aac626ca61c733cf
kgarth <kade.garth@csuglobal.edu>
2025-07-01 23:29:36 -0700

    draft 5

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ def main():
             item1.item_price = float(input('Enter the item price: '))
             if item1.item_price < 0:
                 raise ValueError
+            break
         except ValueError:
             print('Please enter a number greater then 0')
-        break
     
     while True:
         try:
             item1.item_quantity = int(input('Enter the item quantity: '))
             if item1.item_quantity < 0:
                 raise ValueError
+            break
         except ValueError:
             print('Please enter a number greater than 0')
-        break
 
     print('Item2:')
     item2.item_name = input('Enter the item name: ')
@@ -33,18 +33,18 @@ def main():
             item2.item_price = float(input('Enter the item price: '))
             if item2.item_price < 0:
                 raise ValueError
+            break
         except ValueError:
             print('Please enter a number greater then 0')
-        break
     
     while True:
         try:
             item2.item_quantity = int(input('Enter the item quantity: '))
             if item2.item_quantity < 0:
                 raise ValueError
+            break
         except ValueError:
             print('Please enter a number greater than 0')
-        break
 
     print('Total Cost:')
     item1.print_item_cost()
